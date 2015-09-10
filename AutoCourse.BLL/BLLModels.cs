25cd4ac7aff39496e143143703aa78f7169759cc
庄严 <liu_liu66@sina.com>
2015-09-10 10:32:57 +0800

    OK

---
+++ AutoCourse.BLL/BLLModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoCourse.Models;
+
+namespace AutoCourse.BLL
+{
+    public class BLLClass : ACBaseServices<Class>
+    {
+
+    }
+
+    public class BLLCousrse : ACBaseServices<Course>
+    {
+    }
+
+    public class BLLManageUser : ACBaseServices<ManageUser>
+    {
+    }
+
+    public class BLLSchool : ACBaseServices<School>
+    {
+    }
+
+    public class BllTeacher : ACBaseServices<Teacher>
+    {
+
+    }
+
+    public class BLLTeacherCourse : ACBaseServices<TeacherCourseClass>
+    {
+
+    }
+
+    public class BLLTeacherCourseClass : ACBaseServices<TeacherCourseClass>
+    {
+
+    }
+}
