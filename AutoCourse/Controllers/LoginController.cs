39c07750b9bf04da26f9f1d5b03b06b4cebf2ae2
庄严 <liu_liu66@sina.com>
2015-09-11 18:00:35 +0800

    UserLogin

--- AutoCourse/Controllers/LoginController.cs
+++ AutoCourse/Controllers/LoginController.cs
@@ -3,24 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoCourse.BLL;
 
 namespace AutoCourse.Controllers
 {
     public class LoginController : Controller
     {
+
         //
         // GET: /Login/
         public ActionResult Index()
         {
+            BLLManageUser mu = new BLLManageUser();
+            ViewData["username"] = mu.GetUserName(User);
+            ViewData["schoolid"] = mu.GetSchoolID(User);
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(string username)
+        public ActionResult Login()
         {
-            AutoCourse.BLL.BLLManageUser m = new BLL.BLLManageUser();
-            AutoCourse.Models.ManageUser mu = m.Find(username);
-            
+            string username = Request.Form["tbusername"];
+
+            BLLManageUser mu = new BLLManageUser();
+            AutoCourse.Models.ManageUser m = mu.Find(u => u.UserName == username);
+            mu.Authentication(Request.RequestContext.HttpContext, username, m);
+
+            ViewData["username"] = mu.GetUserName(User);
+            ViewData["schoolid"] = mu.GetSchoolID(User);
+
+            return View("index");
+            //return Redirect("~/School/index");
         }
-	}
+
+        public ActionResult LoginOut()
+        {
+            BLLManageUser.LoginOut(Request.RequestContext.HttpContext);
+
+            BLLManageUser mu = new BLLManageUser();
+            string s = mu.GetUserName(User) + "|" + mu.GetSchoolID(User);
+            return Content(s);
+        }
+    }
 }
---
+++ AutoCourse/Controllers/SchoolController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoCourse.BLL;
+
+namespace AutoCourse.Controllers
+{
+    [Authorize]
+    public class SchoolController : Controller
+    {
+        //
+        // GET: /School/
+        public ActionResult Index()
+        {
+            return View(new BLLSchool().FindList(m => true));
+        }
+
+        public ActionResult CreateNew()
+        {
+            return View();
+        }
+    }
+}
